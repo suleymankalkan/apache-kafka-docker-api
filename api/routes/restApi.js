a5cc711cf076a62a7aed7dd1f0e4b8bf011b2f30
Suleyman Kalkan <skalkan.dev@gmail.com>
2021-03-26 14:24:16 +0300

    logging operations will be handled by logger middleware.

--- api/routes/restApi.js
+++ api/routes/restApi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = require('express').Router();
-const requestLogger = require('../logger/middleware');
+const requestLogger = require('../middleware/logger');
 /* 
 The API Requirements
     It should return a successful reponse in 0-3 seconds (randomize the response time)
@@ -21,39 +21,13 @@ router.post('/', requestLogger,  async(req, res) => {
 
 /* PUT Method */
 /* It should be able to accept empty request body*/
-router.put('/', async(req, res) => {
-    let responseTime = await getRandomIntInclusive(0,3000);
-    let timeStamp = moment().format();
-    let logRes = {
-        "statusCode" : "200",
-        "methodType" : "PUT",
-        "responseTime" : responseTime,
-        "timeStamp" : timeStamp
-    };
-    if(!(Object.keys(req.body).length === 0 && req.body.constructor === Object))
-        logRes["reqBody"] = req.body;
-    await logger.log({
-        level: 'info',
-        message: `${logRes.methodType},${logRes.responseTime},${logRes.timeStamp}`
-      });
-    await setTimeout((()=>{res.json(logRes)}), responseTime);
+router.put('/', requestLogger, async(req, res) => {
+    setTimeout((()=>{res.json(req.log)}), req.log.responseTime);
 });
 
 /* DELETE Method */
-router.delete('/', async(req, res) => {
-    let responseTime = await getRandomIntInclusive(0,3000);
-    let timeStamp = moment().format();
-    let logRes = {
-        "statusCode" : "200",
-        "methodType" : "DELETE",
-        "responseTime" : responseTime,
-        "timeStamp" : timeStamp
-    };
-    await logger.log({
-        level: 'info',
-        message: `${logRes.methodType},${logRes.responseTime},${logRes.timeStamp}`
-      });
-    await setTimeout((()=>{res.json(logRes)}), responseTime);
+router.delete('/', requestLogger, async(req, res) => {
+    setTimeout((()=>{res.json(req.log)}), req.log.responseTime);
 });
 
 module.exports = router;
