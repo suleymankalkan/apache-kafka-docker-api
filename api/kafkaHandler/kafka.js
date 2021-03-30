4a053031718bc9e2e3dd7b048c2e72e9f17a2853
Suleyman Kalkan <skalkan.dev@gmail.com>
2021-03-30 21:02:24 +0300

    test

--- api/kafkaHandler/kafka.js
+++ api/kafkaHandler/kafka.js
@@ -2,23 +2,40 @@ const Tail = require('tail').Tail;
 const {Kafka} = require('kafkajs');
 
 const tail = new Tail("api.log", {fromBeginning: false, useWatchFile: true});
-/*const kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['kafka1:9092', 'kafka2:9092']
-  })
-const producer = kafka.producer();
-const consumer = kafka.consumer();*/
 
-tail.on('line', (line) => {
+const kafka = new Kafka({
+    clientId: 'kartaca-log',
+    brokers: ['kafka:9092'],
+    retry: {
+        retries: 10
+    }
+  });
+
+    const admin = kafka.admin();
+    const producer = kafka.producer();
+
+/*const producer = kafka.producer();
+const consumer = kafka.consumer();
+*/
+/*tail.on('line', (line) => {
     console.log("yeni log:", line);
-})
+})*/
 
-/*
+
 const produceLogs = async () => {
+    await admin.connect();
     await producer.connect();
+    let topics = await admin.listTopics();
+    if (topics.includes('api-log') == false) {
+        await admin.createTopics({
+            topics: [{
+                topic: 'api-log'
+            }]
+        });
+    }
     tail.on('line', (line) => {
         producer.send({
-            topic: 'logs',
+            topic: 'api-log',
             messages: [
                 { value: line },
             ],
@@ -26,6 +43,7 @@ const produceLogs = async () => {
         console.log("yeni log:", line);
     })
 };
+
 
 const consumeLogs = async () => {
     await consumer.connect()
@@ -36,4 +54,6 @@ const consumeLogs = async () => {
         await MONGO_INSERT_BLA_BLA()
       }
     })
-};*/
+};
+
+produceLogs();
