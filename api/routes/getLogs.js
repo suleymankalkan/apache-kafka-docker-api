4af8befd350aaab1047dfc5de84a284b938369ea
Suleyman Kalkan <skalkan.dev@gmail.com>
2021-03-31 17:50:16 +0300

    getLogs route added.

--- api/routes/getLogs.js
+++ api/routes/getLogs.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const LogSchema = require('../models/logSchema');
 
 /* GET logs from mongodb. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    const oneHourBefore = Date.now() - (3600 * 1000);
+    LogSchema.find({timeStamp : { $gte : oneHourBefore }}).sort({timeStamp: -1}).exec((err, data) => { 
+        res.send(data);    
+    });
+    console.log("Current time:", Date.now())
 });
 
 module.exports = router;
