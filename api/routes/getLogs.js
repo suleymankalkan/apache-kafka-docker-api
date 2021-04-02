65f70b394178b9b6fbced81dca0d2c716d738601
Suleyman Kalkan <skalkan.dev@gmail.com>
2021-04-02 12:59:52 +0300

    sort changed.

--- api/routes/getLogs.js
+++ api/routes/getLogs.js
@@ -6,7 +6,7 @@ const LogSchema = require('../models/logSchema');
 /* GET logs from mongodb. */
 router.get('/', function(req, res, next) {
     const oneHourBefore = Date.now() - (3600 * 1000);
-    LogSchema.find({timeStamp : { $gte : oneHourBefore }}).sort({timeStamp: -1}).exec((err, data) => { 
+    LogSchema.find({timeStamp : { $gte : oneHourBefore }}).sort({timeStamp: 1}).exec((err, data) => { 
         res.send(data);    
     });
     console.log("Current time:", Date.now())
